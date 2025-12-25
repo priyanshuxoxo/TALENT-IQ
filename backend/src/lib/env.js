eb00c065608e38a21143b29a6755296b8ba69b5b
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2025-12-26 02:05:09 +0530

    Revert "fix:express v5 spa fallback route"
    This reverts commit dc0838061ab247a6b2e53797dd3439b6a5470e1b.

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,12 +1,7 @@
 import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({
-  path: path.resolve(process.cwd(), "backend/.env"),
-});
-
+dotenv.config();
 export const ENV = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
-  NODE_ENV: process.env.NODE_ENV || "development",
+  NODE_ENV: process.env.NODE_ENV,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,28 +3,23 @@ import path from "path";
 import { ENV } from "./lib/env.js";
 
 const app = express();
-const ROOT_DIR = path.resolve();
-
-// API routes
+const __dirname = path.resolve();
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is running" });
 });
-
 app.get("/books", (req, res) => {
   res.status(200).json({ message: "this is the books endpoint" });
 });
 
-// Serve frontend (SPA)
-const frontendPath = path.join(ROOT_DIR, "frontend", "dist");
+//make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-app.use(express.static(frontendPath));
-
-app.use((req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
+app.listen(ENV.PORT, () => {
+  console.log(`Server is listening on port ${ENV.PORT}`);
 });
-
-const PORT = ENV.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+export default app;
