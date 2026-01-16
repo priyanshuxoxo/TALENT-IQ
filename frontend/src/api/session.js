27f324b843e685d89e44275fbd23e2c074ccbeb7
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2026-01-17 01:08:02 +0530

    dasdboard page added

---
+++ frontend/src/api/session.js
@@ -0,0 +1,31 @@
+import axiosInstance from "../lib/axios.js";
+export const sessionApi = {
+  createSession: async (data) => {
+    const response = await axiosInstance.post("/sessions", data);
+    return response.data;
+  },
+  getActiveSessions: async () => {
+    const response = await axiosInstance.get("/sessions/active");
+    return response.data;
+  },
+  getMyRecentSessions: async () => {
+    const response = await axiosInstance.get("/sessions/my-recent");
+    return response.data;
+  },
+  getSessionById: async (id) => {
+    const response = await axiosInstance.get(`/sessions/active${id}`);
+    return response.data;
+  },
+  joinSession: async (id) => {
+    const response = await axiosInstance.post(`/sessions/active${id}/join`);
+    return response.data;
+  },
+  endSession: async (id) => {
+    const response = await axiosInstance.post(`/sessions/active${id}/end`);
+    return response.data;
+  },
+  getStreamToken: async () => {
+    const response = await axiosInstance.get(`/chat/token`);
+    return response.data;
+  },
+};
