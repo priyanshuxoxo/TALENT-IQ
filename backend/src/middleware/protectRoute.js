a8b8286b0aa2aa062740fc905fa4a2df7f93e087
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2026-01-18 13:38:08 +0530

    Revert "update cookies storage"
    This reverts commit e5a5c38f35313889ccb56ea675e302bc6fc75d06.

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,23 +1,23 @@
-import { getAuth } from "@clerk/express";
+import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
-export const protectRoute = async (req, res, next) => {
-  try {
-    const { userId } = getAuth(req);
-
-    if (!userId) {
-      return res.status(401).json({ message: "Unauthorized" });
+export const protectRoute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
+      if (!clerkId) {
+        return res.status(401).json({ message: "Unauthorized-invalid token" });
+      }
+      const user = await User.findOne({ clerkId });
+      if (!user) {
+        return res.status(404).json({ message: "Unauthorized-user not found" });
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      console.error("Error in protectroute middleware", error);
+      res.status(500).json({ message: "Internal server error" });
     }
-
-    const user = await User.findOne({ clerkId: userId });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    console.error("Error in protectRoute:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+  },
+];
